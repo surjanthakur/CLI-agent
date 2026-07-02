0fbf994bc92ecfea48e2e22db76784a67edc1b31
surjanthakur <tsurjan506@gmail.com>
2026-07-03 00:20:33 +0530

    s

--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
-from ast import arguments
 import typer
 
-app = typer.Typer()
-
-
-@app.command()
-def greet(
-    name: str = typer.Argument(help="this shows you name"),
-    age: int = typer.Option(min=10, help="this shows your age"),
-):
-    print(f"{name} and the age is {age}")
+app = typer.Typer(name="nova")
 
 
 app()
