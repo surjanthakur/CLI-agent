ea5df16fe9af0be14a2a40f522f2c7694f60414b
surjanthakur <tsurjan506@gmail.com>
2026-07-05 18:50:51 +0530

    s

--- app/main.py
+++ app/main.py
@@ -1,17 +1,1 @@
-import typer
-from requests import request
-
-app = typer.Typer(name="nova")
-
-
-@app.command("weather")
-def check_weather(city: str = typer.Option(...)):
-    req = request(
-        method="GET",
-        url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=04686a7ac53877a0115b545489c9fac1&units=metric",
-    )
-    if req.status_code == 200:
-        data = req.json()
-
-
-app()
+import subprocess
