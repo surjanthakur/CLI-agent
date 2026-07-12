e9c0ccb2c412e125311213012e05ed08baa8df32
surjanthakur <tsurjan506@gmail.com>
2026-07-13 00:15:45 +0530

    s

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ import subprocess
 
 print("a")  # 1
 
-res = subprocess.Popen(["python3", "--version"], text=True)
+res = subprocess.Popen(
+    ["python3", "--version"],
+    text=True,
+)
 
-res.stdout  # 3
+res.stdout
 
 print("b")  # 2
