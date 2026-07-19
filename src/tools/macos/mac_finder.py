58c1ea383ff4e6f1b63051eb2b6c78ea7beecd82
surjanthakur <tsurjan506@gmail.com>
2026-07-19 11:22:01 +0530

    s

--- src/tools/macos/mac_finder.py
+++ src/tools/macos/mac_finder.py
@@ -1,26 +1,29 @@
 from common import subprocess
 
 
-# open mac app func
+# open mac app
 def open_app(app_name: str):
     """this function open the mac apps"""
     script = f'tell application "{app_name}" to activate'
     subprocess.run_process(script)
 
 
-# close mac app func
+# close mac app
 def close_app(app_name: str):
     """this function close the mac apps"""
     script = f'tell application "{app_name}" to activate'
     subprocess.run_process(script)
 
 
+# hide mac app
 def hide_app(app_name: str):
     """this function hide the mac apps"""
     script = f"tell application {app_name} hide end tell"
     subprocess.run_process(script)
 
 
+# unhide mac app
 def unhide_app(app_name: str):
+    """this function unhide the mac apps"""
     script = f'tell application "{app_name}" to activate'
     subprocess.run_process(script)
