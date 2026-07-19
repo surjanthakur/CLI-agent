3ef09f900daefa7000ef8327e002c213ff61ece2
surjanthakur <tsurjan506@gmail.com>
2026-07-19 18:22:49 +0530

    s

--- src/commands/app.py
+++ src/commands/app.py
@@ -1,12 +1,11 @@
 from .mac_apps import open_command, close_command, hide_command, unhide_command
-from .chat import chat_command
 
 import typer
 
 app = typer.Typer()
 
+# mac apps commands
 app.command(name="open")(open_command)
 app.command(name="close")(close_command)
 app.command(name="hide")(hide_command)
 app.command(name="unhide")(unhide_command)
-app.command(name="chat")(chat_command)
