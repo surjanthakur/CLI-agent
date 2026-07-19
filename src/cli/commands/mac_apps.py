727bf2bba1025bbd00ea7f842448521c0c4d5aeb
surjanthakur <tsurjan506@gmail.com>
2026-07-19 14:34:08 +0530

    Add commands to manage macOS applications: close, hide, and unhide

--- src/cli/commands/mac_apps.py
+++ src/cli/commands/mac_apps.py
@@ -9,3 +9,18 @@ app = typer.Typer()
 @app.command(name="open", help="command helps to open mac apps")
 def open(name: Annotated[str, typer.Argument()]):
     mac_finder.open_app(app_name=name)
+
+
+@app.command(name="close")
+def close(name: Annotated[str, typer.Argument()]):
+    mac_finder.close_app(app_name=name)
+
+
+@app.command(name="hide")
+def close(name: Annotated[str, typer.Argument()]):
+    mac_finder.hide_app(app_name=name)
+
+
+@app.command(name="unhide")
+def close(name: Annotated[str, typer.Argument()]):
+    mac_finder.unhide_app(app_name=name)
