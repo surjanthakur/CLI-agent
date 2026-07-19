1a7483f05b10d6815b3e08396e00bc888e1a3644
surjanthakur <tsurjan506@gmail.com>
2026-07-19 15:30:17 +0530

    s

--- src/cli/commands/mac_apps.py
+++ src/cli/commands/mac_apps.py
@@ -7,20 +7,24 @@ app = typer.Typer()
 
 
 @app.command(help="command helps to open mac apps")
-def open_command(name: Annotated[str, typer.Argument()]):
-    mac_finder.open_app(app_name=name)
+def open_command(name: list[str] = typer.Argument(...)):
+    extract_name = " ".join(name)
+    mac_finder.open_app(app_name=extract_name)
 
 
 @app.command()
-def close_command(name: Annotated[str, typer.Argument()]):
-    mac_finder.close_app(app_name=name)
+def close_command(name: list[str] = typer.Argument(...)):
+    extract_name = " ".join(name)
+    mac_finder.close_app(app_name=extract_name)
 
 
 @app.command()
-def hide_command(name: Annotated[str, typer.Argument()]):
-    mac_finder.hide_app(app_name=name)
+def hide_command(name: list[str] = typer.Argument(...)):
+    extract_name = " ".join(name)
+    mac_finder.hide_app(app_name=extract_name)
 
 
 @app.command()
-def unhide_command(name: Annotated[str, typer.Argument()]):
-    mac_finder.unhide_app(app_name=name)
+def unhide_command(name: list[str] = typer.Argument(...)):
+    extract_name = " ".join(name)
+    mac_finder.unhide_app(app_name=extract_name)
