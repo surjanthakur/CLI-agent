4899d38cce4f486808e3d279ba98a7140c4a2567
surjanthakur <tsurjan506@gmail.com>
2026-07-24 15:54:33 +0530

    s

--- cli/core/config.py
+++ cli/core/config.py
@@ -6,6 +6,12 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
+        env_ignore_empty=True,
         case_sensitive=False,
         extra="ignore",
     )
+    PAPERTRAIL_HOST: str
+    PAPERTRAIL_TOKEN: str
+
+
+settings = Settings()
--- cli/core/logging.py
+++ cli/core/logging.py
@@ -1,10 +1,16 @@
 import logging
+from logging.handlers import SysLogHandler
+
+from ..core.config import settings
 
 logging.basicConfig(
     filename="app.log",
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(message)s",
+    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger(__name__)
+
+handler = SysLogHandler(address=(settings.PAPERTRAIL_HOST, settings.PAPERTRAIL_TOKEN))
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
