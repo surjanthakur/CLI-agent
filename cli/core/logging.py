934c1d81dc6ed09052826aca9b2c0b99aaec8bfa
surjanthakur <tsurjan506@gmail.com>
2026-07-24 17:19:40 +0530

    s

--- cli/core/logging.py
+++ cli/core/logging.py
@@ -4,16 +4,13 @@ import sentry_sdk
 
 from .config import settings
 
-# from .config import settings
-
 sentry_sdk.init(
     dsn=settings.SENTRY_DNS,
     enable_logs=True,
 )
 
 logging.basicConfig(
-    level=logging.DEBUG,
-    filename="app.log",
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
