21afd79d8caaf517596d8b55bb649b101072a4b2
surjanthakur <tsurjan506@gmail.com>
2026-07-25 13:00:38 +0530

    Refactor logging configuration and enable Sentry SDK integration

--- src/neo/core/logging.py
+++ src/neo/core/logging.py
@@ -1,19 +1,16 @@
 import logging
 
+import sentry_sdk
+
 from .config import settings
 
-# import sentry_sdk
-
-# from .config import settings
-
-# sentry_sdk.init(
-#     dsn=settings.SENTRY_DNS,
-#     enable_logs=True,
-# )
+sentry_sdk.init(
+    dsn=settings.SENTRY_DNS,
+    enable_logs=True,
+)
 
 logging.basicConfig(
     level=settings.LOGS_LEVEL,
-    filename="app.log",
     format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
